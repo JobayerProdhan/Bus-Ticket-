2e27542f3dd686d6471a7cecf4ce314a9f3578fb
Jobayer Prodhan <jobayerprodhan08@gmail.com>
2025-03-09 04:10:08 +0600

    setup nav banner card and coupon section done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}", // Adjust this based on your project structure
+    ],
+    theme: {
+      extend: {}, // Customize your design system here
+    },
+    plugins: [], // Add Tailwind plugins if needed
+  };
+  
